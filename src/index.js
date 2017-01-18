bb8ba6d11a16686039756011c2d630c32e75bf84
Sebastian Becker-Rakow <SebastianBeckerRakow@users.noreply.github.com>
2017-01-18 14:03:42 +0100

    changes for soccer version

--- src/index.js
+++ src/index.js
@@ -13,83 +13,83 @@ var numbers = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "
 var languageString = {
     "en-GB": {
         "translation": {
-            "GAME_NAME" : "office supply memory", // Be sure to change this for your skill.
-            "HELP_MESSAGE": "I will give you up to %s different office supplies. You have to repeat them. " +
+            "GAME_NAME" : "soccer team memory for the german bundesliga", // Be sure to change this for your skill.
+            "HELP_MESSAGE": "I will give you up to %s different teams from the german bundesliga. You have to repeat them. " +
             "To start a new game at any time, say, start game. ",
-            "REPEAT_QUESTION_MESSAGE": "To repeat the last office supply, say, repeat. ",
+            "REPEAT_QUESTION_MESSAGE": "To repeat the last team, say, repeat. ",
             "ASK_MESSAGE_START": "Would you like to start playing?",
-            "HELP_REPROMPT": "To get to the next stage, repeat all the supplies I already gave to you. ",
+            "HELP_REPROMPT": "To get to the next stage, repeat all the names of the teams I already gave to you. ",
             "STOP_MESSAGE": "Would you like to keep playing?",
             "CANCEL_MESSAGE": "Ok, let\'s play again soon.",
             "NO_MESSAGE": "Ok, we\'ll play another time. Goodbye!",
-            "TRIVIA_UNHANDLED": "Try saying the office supplies",
+            "TRIVIA_UNHANDLED": "Try saying the name of the bundesliga teams",
             "HELP_UNHANDLED": "Say yes to continue, or no to end the game.",
             "START_UNHANDLED": "Say start to start a new game.",
             "NEW_GAME_MESSAGE": "Welcome to %s. ",
-            "WELCOME_MESSAGE": "I will give you up to %s different office supplies. You have to repeat them. " +
+            "WELCOME_MESSAGE": "I will give you up to %s different teams from the german bundesliga. You have to repeat them. " +
             "Let\'s begin with level 1. ",
             "ANSWER_CORRECT_MESSAGE": "correct. ",
             "ANSWER_WRONG_MESSAGE": "wrong. ",
-            "CORRECT_ANSWER_MESSAGE": "The correct office supply is %s. ",
+            "CORRECT_ANSWER_MESSAGE": "The correct team is %s. ",
             "ANSWER_IS_MESSAGE": "That answer is ",
-            "TELL_QUESTION_MESSAGE": "office supply %s. %s ",
-            "GAME_OVER_MESSAGE": "You got %s out of %s office supplies correct. Thank you for playing!",
-            "SCORE_IS_MESSAGE": "You remembered %s office supplies correctly",
-            "NEXT_LEVEL_MESSAGE": "At the next level you have to remeber %s office supplies!"
+            "TELL_QUESTION_MESSAGE": "Team %s. %s ",
+            "GAME_OVER_MESSAGE": "You got %s out of %s teams correct. Thank you for playing!",
+            "SCORE_IS_MESSAGE": "You remembered %s teams correctly",
+            "NEXT_LEVEL_MESSAGE": "At the next level you have to remeber %s teams!"
         }
     },
     "en-US": {
         "translation": {
-            "GAME_NAME" : "office supply memory", // Be sure to change this for your skill.
-            "HELP_MESSAGE": "I will give you up to %s different office supplies. You have to repeat them. " +
+            "GAME_NAME" : "soccer team memory for the german bundesliga", // Be sure to change this for your skill.
+            "HELP_MESSAGE": "I will give you up to %s different teams from the german bundesliga. You have to repeat them. " +
             "To start a new game at any time, say, start game. ",
-            "REPEAT_QUESTION_MESSAGE": "To repeat the last office supply, say, repeat. ",
+            "REPEAT_QUESTION_MESSAGE": "To repeat the last team, say, repeat. ",
             "ASK_MESSAGE_START": "Would you like to start playing?",
-            "HELP_REPROMPT": "To get to the next stage, repeat all the supplies I already gave to you. ",
+            "HELP_REPROMPT": "To get to the next stage, repeat all the names of the teams I already gave to you. ",
             "STOP_MESSAGE": "Would you like to keep playing?",
             "CANCEL_MESSAGE": "Ok, let\'s play again soon.",
             "NO_MESSAGE": "Ok, we\'ll play another time. Goodbye!",
-            "TRIVIA_UNHANDLED": "Try saying the office supplies",
+            "TRIVIA_UNHANDLED": "Try saying the name of the bundesliga teams",
             "HELP_UNHANDLED": "Say yes to continue, or no to end the game.",
             "START_UNHANDLED": "Say start to start a new game.",
             "NEW_GAME_MESSAGE": "Welcome to %s. ",
-            "WELCOME_MESSAGE": "I will give you up to %s different office supplies. You have to repeat them. " +
+            "WELCOME_MESSAGE": "I will give you up to %s different teams from the german bundesliga. You have to repeat them. " +
             "Let\'s begin with level 1. ",
             "ANSWER_CORRECT_MESSAGE": "correct. ",
             "ANSWER_WRONG_MESSAGE": "wrong. ",
-            "CORRECT_ANSWER_MESSAGE": "The correct office supply is %s. ",
+            "CORRECT_ANSWER_MESSAGE": "The correct teams are %s. ",
             "ANSWER_IS_MESSAGE": "That answer is ",
-            "TELL_QUESTION_MESSAGE": "office supply %s. %s ",
-            "GAME_OVER_MESSAGE": "You got %s out of %s office supplies correct. Thank you for playing!",
-            "SCORE_IS_MESSAGE": "You remembered %s office supplies correctly",
-            "NEXT_LEVEL_MESSAGE": "At the next level you have to remeber %s office supplies!"
+            "TELL_QUESTION_MESSAGE": "Team %s. %s ",
+            "GAME_OVER_MESSAGE": "You got %s out of %s teams correct. Thank you for playing!",
+            "SCORE_IS_MESSAGE": "You remembered %s teams correctly",
+            "NEXT_LEVEL_MESSAGE": "At the next level you have to remember %s teams!"
         }
     },
     "de-DE": {
         "translation": {
-            "GAME_NAME" : "Büroartikel Memory", // Be sure to change this for your skill.
-            "HELP_MESSAGE": "Ich nenne dir bis zu %s verschiedene Büroartikel. Du musst diese wiederholen. " +
+            "GAME_NAME" : "Fußball Team Memory für die deutsche Bundesliga", // Be sure to change this for your skill.
+            "HELP_MESSAGE": "Ich nenne dir bis zu %s verschiedene Teams aus der deutschen Bundesliga. Du musst diese wiederholen. " +
             "Um ein neues Spiel zu starten, sage einfach „Spiel starten“. ",
             "REPEAT_QUESTION_MESSAGE": "Wenn die letzte Frage wiederholt werden soll, sage „Wiederholen“ ",
             "ASK_MESSAGE_START": "Möchten Sie beginnen?",
-            "HELP_REPROMPT": "Um in das nächste Level zu kommen, musst du alle Büroartikel wiederholen, die ich dir genannt habe. ",
+            "HELP_REPROMPT": "Um in das nächste Level zu kommen, musst du alle Namen der Teams wiederholen, die ich dir genannt habe. ",
             "STOP_MESSAGE": "Möchtest du weiterspielen?",
             "CANCEL_MESSAGE": "OK, dann lass uns bald mal wieder spielen.",
             "NO_MESSAGE": "OK, spielen wir ein andermal. Auf Wiedersehen!",
-            "TRIVIA_UNHANDLED": "Versuch dich an ein Büroartikel zu erinnern.",
+            "TRIVIA_UNHANDLED": "Versuch dich an ein Team zu erinnern.",
             "HELP_UNHANDLED": "Sage ja, um fortzufahren, oder nein, um das Spiel zu beenden.",
             "START_UNHANDLED": "Du kannst jederzeit ein neues Spiel beginnen, sage einfach „Spiel starten“.",
             "NEW_GAME_MESSAGE": "Willkommen bei %s. ",
-            "WELCOME_MESSAGE": "Ich nenne dir bis zu %s verschiedene Büroartikel. Du musst diese wiederholen.  " +
+            "WELCOME_MESSAGE": "Ich nenne dir bis zu %s verschiedene Teams aus der deutschen Bundesliga. Du musst diese wiederholen.  " +
             "Lass uns mit Level eins starten. ",
             "ANSWER_CORRECT_MESSAGE": "richtig. ",
             "ANSWER_WRONG_MESSAGE": "falsch. ",
-            "CORRECT_ANSWER_MESSAGE": "Die richtigen Büroartikel waren %s. ",
+            "CORRECT_ANSWER_MESSAGE": "Die richtigen Teams waren %s. ",
             "ANSWER_IS_MESSAGE": "Die Antwort ist ",
-            "TELL_QUESTION_MESSAGE": "Büroartikel %s. %s ",
-            "GAME_OVER_MESSAGE": "Du hast dir %s von %s Büroartikel richtig gemerkt. Danke fürs Spielen!",
-            "SCORE_IS_MESSAGE": "Du hast dir %s Büroartikel richtig gemerkt",
-            "NEXT_LEVEL_MESSAGE": "Im nächsten Level musst du dir %s Büroartikel merken!"
+            "TELL_QUESTION_MESSAGE": "Team %s. %s ",
+            "GAME_OVER_MESSAGE": "Du hast dir %s von %s Teams richtig gemerkt. Danke fürs Spielen!",
+            "SCORE_IS_MESSAGE": "Du hast dir %s Teams richtig gemerkt",
+            "NEXT_LEVEL_MESSAGE": "Im nächsten Level musst du dir %s Teams merken!"
         }
     }
 };
@@ -129,21 +129,21 @@ var startStateHandlers = Alexa.CreateStateHandler(GAME_STATES.START, {
     "StartGame": function (newGame) {
         var speechOutput = newGame ? this.t("NEW_GAME_MESSAGE", this.t("GAME_NAME")) + this.t("WELCOME_MESSAGE", GAME_LENGTH.toString()) : "";
         // Select GAME_LENGTH questions for the game
-        var officeSupplies = generateOfficeSuppliesOrdering();
+        var soccerClubs = generateSoccerClubsOrdering();
         // Select and shuffle the answers for each question
 
-        var currentSupplyIndex = 0;
-        var spokenOfficeSupply = officeSupplies[currentSupplyIndex];
-        var repromptText = this.t("TELL_QUESTION_MESSAGE", "1", spokenOfficeSupply);
+        var currentClubIndex = 0;
+        var spokenSoccerClub = soccerClubs[currentClubIndex];
+        var repromptText = this.t("TELL_QUESTION_MESSAGE", "1", spokenSoccerClub);
 
         speechOutput += repromptText;
 
         Object.assign(this.attributes, {
             "speechOutput": repromptText,
             "repromptText": repromptText,
-            "officeSupplies": officeSupplies,
+            "soccerClubs": soccerClubs,
             "score": 0,
-            "currentSupplyIndex": 0
+            "currentTeamIndex": 0
         });
 
         // Set the current state to trivia mode. The skill will now use handlers defined in triviaStateHandlers
@@ -153,7 +153,7 @@ var startStateHandlers = Alexa.CreateStateHandler(GAME_STATES.START, {
 });
 
 var triviaStateHandlers = Alexa.CreateStateHandler(GAME_STATES.TRIVIA, {
-    "SupplyIntent": function () {
+    "TeamIntent": function () {
         handleUserGuess.call(this, false);
     },
     "AMAZON.StartOverIntent": function () {
@@ -238,77 +238,88 @@ function handleUserGuess(userGaveUp) {
     var speechOutput = "";
     var speechOutputAnalysis = "";
 
-    var officeSupplies = this.attributes.officeSupplies;
+    var soccerClubs = this.attributes.soccerClubs;
     var currentScore = parseInt(this.attributes.score);
-    var currentSupplyIndex = parseInt(this.attributes.currentSupplyIndex);
+    var currentTeamIndex = parseInt(this.attributes.currentTeamIndex);
     var repromptText = this.attributes.repromptText;
-    var spokenOfficeSupply = "";
-    var correctOfficeSupplies = "";
+    var spokenSoccerClub = "";
+    var correctSoccerTeams = "";
     var newQuestions = false;
 
-    var ok =  this.event.request.intent.slots["Supply"].value.toLowerCase() == officeSupplies[currentSupplyIndex].toLowerCase();
+    console.log("guessedsoccerClub: " + this.event.request.intent.slots["Team"].value.toLowerCase());
+    console.log("wantedsoccerClub: " + soccerClubs[currentTeamIndex].toLowerCase());
+    var ok =  this.event.request.intent.slots["Team"].value.toLowerCase() == soccerClubs[currentTeamIndex].toLowerCase();
+
+    console.log("ok: " + ok);
 
     // check if user says right order
     if ( ok ) {
 
-        if (currentSupplyIndex == currentScore) {
+        if (currentTeamIndex == currentScore) {
             currentScore++;
             newQuestions = true;
         }
         else {
-            currentSupplyIndex++;
+            currentTeamIndex++;
         }
 
         speechOutputAnalysis = this.t("ANSWER_CORRECT_MESSAGE");
+        console.log("currentTeamIndex: " + currentTeamIndex);
+        console.log("currentScore: " + currentScore);
     } else {
         if (!userGaveUp) {
             speechOutputAnalysis = this.t("ANSWER_WRONG_MESSAGE");
         }
 
         for (var i = 0; (i < currentScore + 1); i++){
-            correctOfficeSupplies += officeSupplies[i] + " and ";
+            correctSoccerTeams += soccerClubs[i] + " and ";
         }
 
-        speechOutputAnalysis += this.t("CORRECT_ANSWER_MESSAGE", correctOfficeSupplies);
-
+        speechOutputAnalysis += this.t("CORRECT_ANSWER_MESSAGE", correctSoccerTeams);
+        console.log("speechOutputAnalysis: " + speechOutputAnalysis);
+        console.log("currentTeamIndex: " + currentTeamIndex);
+        console.log("currentScore: " + currentScore);
     }
 
     // Check if we can exit the game session after we reached GAME_LENGTH
     if (currentScore == GAME_LENGTH || !ok) {
         speechOutput = (userGaveUp || !ok) ? "" : this.t("ANSWER_IS_MESSAGE");
         speechOutput += speechOutputAnalysis + this.t("GAME_OVER_MESSAGE", currentScore.toString(), GAME_LENGTH.toString());
+        console.log("currentScore == GAME_LENGTH: " + speechOutput);
         this.emit(":tell", speechOutput);
     } else {
 
-        // game has not finished yet, so we have to give new supplies to the user
+        // game has not finished yet, so we have to give new clubs to the user
         if (newQuestions) {
             speechOutput += speechOutputAnalysis + this.t("SCORE_IS_MESSAGE", currentScore.toString()) + " " + this.t("NEXT_LEVEL_MESSAGE", currentScore + 1);
 
-            officeSupplies = generateOfficeSuppliesOrdering();
+            soccerClubs = generateSoccerClubsOrdering();
             for (var i = 0; (i < currentScore + 1); i++){
-                spokenOfficeSupply += officeSupplies[i] + " ";
+                spokenSoccerClub += soccerClubs[i] + " ";
             }
 
-            repromptText = spokenOfficeSupply;
+            repromptText = spokenSoccerClub;
 
-            speechOutput += " " + spokenOfficeSupply;
+            speechOutput += " " + spokenSoccerClub;
 
             Object.assign(this.attributes, {
                 "speechOutput": repromptText,
                 "repromptText": repromptText,
-                "officeSupplies": officeSupplies,
+                "soccerClubs": soccerClubs,
                 "score": currentScore,
-                "currentSupplyIndex": 0
+                "currentTeamIndex": 0
             });
+            console.log("currentTeamIndex == currentScore: " + speechOutput);
             this.emit(":askWithCard", speechOutput, repromptText, this.t("GAME_NAME"), repromptText);
         }
         else {
+            console.log("currentTeamIndex != currentScore: " + speechOutput);
             Object.assign(this.attributes, {
                 "speechOutput": repromptText,
                 "repromptText": repromptText,
-                "officeSupplies": officeSupplies,
+                "soccerClubs": soccerClubs,
                 "score": currentScore,
-                "currentSupplyIndex": currentSupplyIndex
+                "currentTeamIndex": currentTeamIndex
             });
             speechOutput += speechOutputAnalysis;
             this.emit(":askWithCard", speechOutput, speechOutput, this.t("GAME_NAME"), speechOutput);
@@ -317,35 +328,26 @@ function handleUserGuess(userGaveUp) {
     }
 }
 
-function generateOfficeSuppliesOrdering() {
-    var officeSupplies = [];
-    var indexList = [
-        "stapler",
-        "staple",
-        "binder clip",
-        "paper clip",
-        "scissor",
-        "ruler",
-        "index card",
-        "staple remover",
-        "paper trimmer",
-        "clipboard",
-        "pen",
-        "notebook",
-        "copy paper",
-        "ink cartridge",
-        "toner cartridge",
-        "diary",
-        "journal",
-        "calender",
-        "marker",
-        "eraser",
-        "binder",
-        "file folder",
-        "file storage",
-        "report covers",
-        "accordion folder"
-    ];
+function generateSoccerClubsOrdering() {
+    var soccerClubs = [];
+    var indexList = ["FC Augsburg",
+        "Hertha BSC",
+        "Werder Bremen",
+        "SV Darmstadt 98",
+        "Borussia Dortmund",
+        "Eintracht Frankfurt",
+        "SC Freiburg",
+        "Hamburger SV",
+        "TSG 1899 Hoffenheim",
+        "FC Ingolstadt 04",
+        "1. FC Köln",
+        "RB Leipzig",
+        "Bayer Leverkusen",
+        "FSV Mainz 05",
+        "Borussia Mönchengladbach",
+        "FC Bayern München",
+        "FC Schalke 04",
+        "VfL Wolfsburg"];
 
     var index = GAME_LENGTH;
 
@@ -357,8 +359,8 @@ function generateOfficeSuppliesOrdering() {
         var temp = indexList[index];
         indexList[index] = indexList[rand];
         indexList[rand] = temp;
-        officeSupplies.push(indexList[index]);
+        soccerClubs.push(indexList[index]);
     }
 
-    return officeSupplies;
+    return soccerClubs;
 }
